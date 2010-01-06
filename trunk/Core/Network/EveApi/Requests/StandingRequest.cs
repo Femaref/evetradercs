b6718b33264bc14486032d774c96630014300801
femaref <femaref@081cbc3e-f01a-11de-acd2-8fd6687fc178>
2010-01-06 13:41:49 +0000

    - fixed a NullReferenceException if standingsTo or standingsFrom does not contain subelements

--- trunk/Core/Network/EveApi/Requests/StandingRequest.cs
+++ trunk/Core/Network/EveApi/Requests/StandingRequest.cs
@@ -30,57 +30,86 @@ namespace Core.Network.EveApi.Requests
         {
             var root = document.Element("eveapi").Element("result");
 
-            var standingsToCharacter =
-                (from d in root.Element("standingsTo").Elements().Where(r => r.Attribute("name").Value == "characters").Descendants()
-                 select new Standing()
-                            {
-                                CharacterID = this.CharacterId,
-                                Target = StandingTarget.Character,
-                                TargetID = int.Parse(d.Attribute("toID").Value),
-                                Type = StandingType.To,
-                                Value = double.Parse(d.Attribute("standing").Value, System.Globalization.CultureInfo.InvariantCulture)
-                            });
-            var standingsToCorporation =
-                (from d in root.Element("standingsTo").Elements().Where(r => r.Attribute("name").Value == "corporations").Descendants()
-                 select new Standing()
-                            {
-                                CharacterID = this.CharacterId,
-                                Target = StandingTarget.Corporation,
-                                TargetID = int.Parse(d.Attribute("toID").Value),
-                                Type = StandingType.To,
-                                Value = double.Parse(d.Attribute("standing").Value)
-                            });
-            var standingsFromAgent =
-                (from d in root.Element("standingsFrom").Elements().Where(r => r.Attribute("name").Value == "agents").Descendants()
-                 select new Standing()
-                            {
-                                CharacterID = this.CharacterId,
-                                Target = StandingTarget.Agent,
-                                TargetID = int.Parse(d.Attribute("fromID").Value),
-                                Type = StandingType.From,
-                                Value = double.Parse(d.Attribute("standing").Value, System.Globalization.CultureInfo.InvariantCulture)
-                            });
-            var standingsFromNPC =
-                (from d in
-                     root.Element("standingsFrom").Elements().Where(r => r.Attribute("name").Value == "NPCCorporations").Descendants()
-                 select new Standing()
-                            {
-                                CharacterID = this.CharacterId,
-                                Target = StandingTarget.NpcCorporation,
-                                TargetID = int.Parse(d.Attribute("fromID").Value),
-                                Type = StandingType.From,
-                                Value = double.Parse(d.Attribute("standing").Value, System.Globalization.CultureInfo.InvariantCulture)
-                            });
-            var standingsFromFaction =
-                (from d in root.Element("standingsFrom").Elements().Where(r => r.Attribute("name").Value == "factions").Descendants()
-                 select new Standing()
-                            {
-                                CharacterID = this.CharacterId,
-                                Target = StandingTarget.Faction,
-                                TargetID = int.Parse(d.Attribute("fromID").Value),
-                                Type = StandingType.From,
-                                Value = double.Parse(d.Attribute("standing").Value, System.Globalization.CultureInfo.InvariantCulture)
-                            });
+            IEnumerable<Standing> standingsToCharacter = new List<Standing>();
+            IEnumerable<Standing> standingsToCorporation = new List<Standing>();
+            IEnumerable<Standing> standingsFromAgent = new List<Standing>();
+            IEnumerable<Standing> standingsFromNPC = new List<Standing>();
+            IEnumerable<Standing> standingsFromFaction = new List<Standing>();
+
+            if (root.Element("standingsTo").HasElements)
+            {
+                standingsToCharacter =
+                    (from d in
+                         root.Element("standingsTo").Elements().Where(r => r.Attribute("name").Value == "characters").
+                         Descendants()
+                     select new Standing()
+                                {
+                                    CharacterID = this.CharacterId,
+                                    Target = StandingTarget.Character,
+                                    TargetID = int.Parse(d.Attribute("toID").Value),
+                                    Type = StandingType.To,
+                                    Value =
+                                        double.Parse(d.Attribute("standing").Value,
+                                                     System.Globalization.CultureInfo.InvariantCulture)
+                                });
+                standingsToCorporation =
+                    (from d in
+                         root.Element("standingsTo").Elements().Where(r => r.Attribute("name").Value == "corporations").
+                         Descendants()
+                     select new Standing()
+                                {
+                                    CharacterID = this.CharacterId,
+                                    Target = StandingTarget.Corporation,
+                                    TargetID = int.Parse(d.Attribute("toID").Value),
+                                    Type = StandingType.To,
+                                    Value = double.Parse(d.Attribute("standing").Value)
+                                });
+            }
+            if (root.Element("standingsFrom").HasElements)
+            {
+                standingsFromAgent =
+                    (from d in
+                         root.Element("standingsFrom").Elements().Where(r => r.Attribute("name").Value == "agents").
+                         Descendants()
+                     select new Standing()
+                                {
+                                    CharacterID = this.CharacterId,
+                                    Target = StandingTarget.Agent,
+                                    TargetID = int.Parse(d.Attribute("fromID").Value),
+                                    Type = StandingType.From,
+                                    Value =
+                                        double.Parse(d.Attribute("standing").Value,
+                                                     System.Globalization.CultureInfo.InvariantCulture)
+                                });
+                standingsFromNPC =
+                    (from d in
+                         root.Element("standingsFrom").Elements().Where(
+                         r => r.Attribute("name").Value == "NPCCorporations").Descendants()
+                     select new Standing()
+                                {
+                                    CharacterID = this.CharacterId,
+                                    Target = StandingTarget.NpcCorporation,
+                                    TargetID = int.Parse(d.Attribute("fromID").Value),
+                                    Type = StandingType.From,
+                                    Value =
+                                        double.Parse(d.Attribute("standing").Value,
+                                                     System.Globalization.CultureInfo.InvariantCulture)
+                                });
+                standingsFromFaction =
+                    (from d in
+                         root.Element("standingsFrom").Elements().Where(r => r.Attribute("name").Value == "factions").
+                         Descendants()
+                     select new Standing()
+                                {
+                                    CharacterID = this.CharacterId,
+                                    Target = StandingTarget.Faction,
+                                    TargetID = int.Parse(d.Attribute("fromID").Value),
+                                    Type = StandingType.From,
+                                    Value =
+                                        double.Parse(d.Attribute("standing").Value,
+                                                     System.Globalization.CultureInfo.InvariantCulture)
+                                });
+            }
 
             return
                 standingsFromAgent.Union(standingsFromFaction).Union(standingsFromNPC).Union(standingsToCharacter).Union
