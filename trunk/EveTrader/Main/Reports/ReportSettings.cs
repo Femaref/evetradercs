f7de758c13afcfb462b21bc6179937dc65d354e1
femaref <femaref@081cbc3e-f01a-11de-acd2-8fd6687fc178>
2010-01-11 14:20:34 +0000

    - forgot to add two files in previous revisions. Sorry.

---
+++ trunk/EveTrader/Main/Reports/ReportSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveTrader.Main.Reports
+{
+    public class ReportSettings
+    {
+        public int ItemsDisplayed { get; set; }
+        public bool AutomaticApply { get; set; }
+        public ReportTimeSetting GraphTimeSpan { get; set; }
+
+        public ReportSettings()
+        {
+            this.ItemsDisplayed = 15;
+            this.AutomaticApply = false;
+            this.GraphTimeSpan = ReportTimeSetting.Week;
+        }
+    }
+}
---
+++ trunk/EveTrader/Main/Reports/ReportTimeSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveTrader.Main.Reports
+{
+    public enum ReportTimeSetting
+    {
+        Week,
+        Month,
+        ThreeMonths,
+        All
+    }
+}
