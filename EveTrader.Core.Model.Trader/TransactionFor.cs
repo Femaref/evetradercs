e242855359f7ddfc06269b3011449ff6794dc426
femaref <femaref@081cbc3e-f01a-11de-acd2-8fd6687fc178>
2010-10-10 12:51:37 +0000

    - moved Model.StaticModel and Model.TraderModel to separate projects

---
+++ EveTrader.Core.Model.Trader/TransactionFor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveTrader.Core.Model
+{
+    public enum TransactionFor : long
+    {
+        Undefined,
+        Personal,
+        Corporation
+    }
+}
