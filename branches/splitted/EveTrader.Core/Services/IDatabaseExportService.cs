e24168cebe93cce4dffc0fe5c276bfdb3048a2cc
femaref <femaref@081cbc3e-f01a-11de-acd2-8fd6687fc178>
2010-11-19 08:08:04 +0000

    - added IDatabaseExportService and IDatabaseImportService to export/import the database/parts of the database to xml - started implementation of DatabaseExportService

---
+++ branches/splitted/EveTrader.Core/Services/IDatabaseExportService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EveTrader.Core.Model.Trader;
+using System.Xml.Linq;
+
+namespace EveTrader.Core.Services
+{
+    public interface IDatabaseExportService
+    {
+        XDocument Export(Accounts a);
+    }
+}
