71e5ac87b85011d5fdcb4b75f81581d1f978204f
femaref <femaref@081cbc3e-f01a-11de-acd2-8fd6687fc178>
2010-11-22 12:41:41 +0000

    - removed tables.sql from EveTrader.Core.Resources - added definition of DatabaseImportService

---
+++ EveTrader.Core/Services/DatabaseImportService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveTrader.Core.Services
+{
+    public class DatabaseImportService : IDatabaseImportService
+    {
+        public void Import(System.Xml.Linq.XDocument xd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
