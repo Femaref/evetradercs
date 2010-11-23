964a68edc2b364e15b62eb188525a20a65054616
femaref <femaref@081cbc3e-f01a-11de-acd2-8fd6687fc178>
2010-11-23 12:11:47 +0000

    - added definitions for IPriceLookup

--- branches/splitted/EveTrader.Core.Interfaces/Services/IPriceLookup.cs
+++ branches/splitted/EveTrader.Core.Interfaces/Services/IPriceLookup.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EveTrader.Core.Model.Trader;
 
 namespace EveTrader.Core.Services
 {
     public interface IPriceLookup
     {
+        decimal Minimum(long typeID, TransactionType mot);
+        decimal Maximum(long typeID, TransactionType mot);
+        decimal Average(long typeID, TransactionType mot);
+        decimal Kurtosis(long typeID, TransactionType mot);
+        decimal Skew(long typeID, TransactionType mot);
+        decimal Variance(long typeID, TransactionType mot);
+        decimal StandardDeviation(long typeID, TransactionType mot);
+        decimal Simulated(long typeID, TransactionType mot);
     }
 }
