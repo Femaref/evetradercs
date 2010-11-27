a879d3851c6591d55158c212320967afb495e52b
femaref <femaref@081cbc3e-f01a-11de-acd2-8fd6687fc178>
2010-11-27 01:29:57 +0000

    - finished DtoGenerator.tt, this should go into the *Request library as well (rewrite is incoming there anyway courtesy of price data grabbing from metrics)

--- branches/splitted/EveTrader.Core/Services/DatabaseImportService.cs
+++ branches/splitted/EveTrader.Core/Services/DatabaseImportService.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
+using EveTrader.Core.Model.Trader;
+using ClassExtenders;
 
 namespace EveTrader.Core.Services
 {
@@ -9,7 +12,54 @@ namespace EveTrader.Core.Services
     {
         public void Import(System.Xml.Linq.XDocument xd)
         {
+            RaiseStarted();
+
+            var root = xd.Root.Element("EveTraderExport");
+
+            foreach (XElement xa in root.Elements("Accounts"))
+            {
+                Accounts a = new Accounts()
+                {
+                    ID = xa.Attribute("userid").Value.ToInt32(),
+                    ApiKey = xa.Attribute("apikey").Value
+                };
+
+                foreach (XElement xe in xa.Elements())
+                {
+                    if (xe.Name == "Characters")
+                        a.Entities.Add(ParseCharacter(xe));
+                    else
+                        a.Entities.Add(ParseCorporation(xe));
+                }
+            }
+
+
+
+            RaiseCompleted();
+        }
+
+        private Entities ParseCorporation(XElement xe)
+        {
             throw new NotImplementedException();
+        }
+
+        private Entities ParseCharacter(XElement xe)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RaiseCompleted()
+        {
+            var handler = Completed;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+
+        private void RaiseStarted()
+        {
+            var handler = Started;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
         }
 
 
